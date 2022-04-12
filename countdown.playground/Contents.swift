9200c43ce7aaaae15dcbeb50d9f76ce4e5cd8cb7
Pulsar <gkals4417@gmail.com>
2022-04-12 22:29:03 +0900

    countNumbers

---
+++ countdown.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+//var userNumber: Int
+
+func counting (userNumber:Int){
+    var num = userNumber
+    while num != 0 {
+        print(num)
+        num -= 1
+    }
+    print("Stop Counting")
+}
+
+counting(userNumber: 10)
+
+
